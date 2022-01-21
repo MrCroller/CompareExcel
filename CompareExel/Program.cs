35e138895f5e9ab804e224e4f6412ad016a79f9c
PVSlavinsky <PVSlavinsky@NB063106>
2022-01-21 18:16:47 +0300

    Добавьте файлы проекта.

---
+++ CompareExel/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Compare;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+    }
+}
