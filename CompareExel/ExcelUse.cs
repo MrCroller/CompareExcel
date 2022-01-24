dd12dfe0fdff6e39106c9b5b4da6b952f9486f6a
PVSlavinsky <PVSlavinsky@NB063106>
2022-01-24 19:13:11 +0300

    Основан класс для работы с Excel

---
+++ CompareExel/ExcelUse.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Office.Interop.Excel;
+
+namespace CompareExcel;
+
+class ExcelUse : IDisposable
+{
+    private Application app;
+    private Workbook workbook;
+    private string? filePatch;
+
+    public ExcelUse()
+    {
+        app = new Application();
+    }
+
+    internal bool Open(string filePatch)
+    {
+        try
+        {
+            workbook = app.Workbooks.Open(filePatch);
+            this.filePatch = filePatch;
+
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    internal void Save(string filePatch)
+    {
+        try
+        {
+            if (!string.IsNullOrEmpty(filePatch))
+            {
+                workbook.SaveAs(filePatch);
+                this.filePatch = null;
+            }
+            else
+            {
+                workbook.Save();
+            }
+            
+        }
+        catch (Exception ex) { Console.WriteLine(ex.Message); }
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            workbook.Close();
+        }
+        catch (Exception ex) { Console.WriteLine(ex.Message); }
+    }
+}
+
--- CompareExel/Program.cs
+++ CompareExel/Program.cs
@@ -1,13 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using ExcelDataReader;
 
-namespace Compare;
+namespace CompareExcel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World!");
+        Stopwatch sw_total = new Stopwatch();
+        sw_total.Start(); // Таймер
+
+        sw_total.Stop();
+    }
+
+    /// <summary>
+    /// Метод читает все файлы в каталоге и возвращает лист таблиц DataTable
+    /// </summary>
+    /// <param name="dirName">Путь к папке</param>
+    /// <returns></returns>
+    private List<DataTable> ReadRange(string dirName)
+    {
+        string[] filesDir = Directory.GetFiles(dirName, "*.xlsx"); // Сканирует файлы формата Excel
+
+        foreach (string filePatch in filesDir)
+        {
+            try
+            {
+                using (var excelApp = new ExcelUse())
+                {
+                    excelApp.Open(filePatch);
+                }
+            }
+            catch (Exception ex) { Console.WriteLine(ex.Message); }
+        }
+
+        return null;
     }
 }
