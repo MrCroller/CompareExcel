6c7e693c3e012ef1a8175ba2b17c11e6d66fd4c8
PVSlavinsky <PVSlavinsky@NB063106>
2022-01-25 18:50:04 +0300

    ExcelUse class update
    Реализованы некоторые функции работы с Excel в классе ExcelUse. Тестируется конвертирование в DataTable

--- CompareExel/ExcelUse.cs
+++ CompareExel/ExcelUse.cs
@@ -1,60 +1,148 @@
-﻿using Microsoft.Office.Interop.Excel;
+﻿using Excel = Microsoft.Office.Interop.Excel;
 
 namespace CompareExcel;
 
 class ExcelUse : IDisposable
 {
-    private Application app;
-    private Workbook workbook;
-    private string? filePatch;
+    private readonly Excel.Application app;
+    /// <summary>
+    /// Рабочая книга
+    /// </summary>
+    public Excel.Workbook Workbook;
+    /// <summary>
+    /// Страница книги Excel
+    /// </summary>
+    private Excel.Worksheet Sheet;
+    private readonly string SheetName = "Лист1";
+
+    /// <summary>
+    /// Путь к файлу
+    /// </summary>
+    internal string? filePatch;
 
     public ExcelUse()
     {
-        app = new Application();
+        app = new Excel.Application
+        {
+            Visible = false
+        };
     }
 
-    internal bool Open(string filePatch)
+    /// <summary>
+    /// Инициализация приложения
+    /// </summary>
+    /// <param name="SheetName">Имя листа</param>
+    public ExcelUse(string SheetName)
+    {
+        app = new Excel.Application();
+        this.SheetName = SheetName;
+    }
+
+    /// <summary>
+    /// Чтение файла из указанного пути (возвращает DataTable)
+    /// </summary>
+    /// <param name="filePatch"></param>
+    /// <returns></returns>
+    public System.Data.DataTable? ReadFile(string filePatch)
     {
         try
         {
-            workbook = app.Workbooks.Open(filePatch);
+            System.Data.DataTable DT = new();
+
+            this.Workbook = app.Workbooks.Open(filePatch);
             this.filePatch = filePatch;
 
+            this.Sheet = Workbook.Worksheets[SheetName];
 
-            return true;
+            // TODO: Создать нормальное формирование таблицы как в методе Convert
+            foreach (System.Data.DataRow row in Sheet.Rows)
+            {
+                DT.Rows.Add(row);
+            }
+
+            return DT;
         }
         catch (Exception ex)
         {
             Console.WriteLine(ex.Message);
-            return false;
+            this.filePatch = null;
+
+            return null;
         }
     }
 
-    internal void Save(string filePatch)
+    /// <summary>
+    /// Сохранение файла
+    /// </summary>
+    public void Save()
     {
         try
         {
-            if (!string.IsNullOrEmpty(filePatch))
-            {
-                workbook.SaveAs(filePatch);
-                this.filePatch = null;
-            }
-            else
-            {
-                workbook.Save();
-            }
-            
+            Workbook.SaveAs(filePatch);
         }
         catch (Exception ex) { Console.WriteLine(ex.Message); }
     }
 
+    /// <summary>
+    /// Сохранение файла в указанный путь
+    /// </summary>
+    /// <param name="fileFolder">Путь к папке</param>
+    /// <param name="fileName">Имя файла</param>
+    public void SaveAs(string fileFolder, string fileName)
+    {
+        filePatch = $@"{fileFolder}/{fileName}";
+        Save();
+    }
+
+    /// <summary>
+    /// Преобразование DataTable в рабочую страницу
+    /// </summary>
+    /// <param name="DT"></param>
+    public void Convert(System.Data.DataTable DT)
+    {
+        try
+        {
+            // Именование колонок
+            char _excelHeader = 'A';
+            foreach (System.Data.DataColumn column in DT.Columns)
+            {
+                Sheet.Cells[1, _excelHeader.ToString()] = column.ColumnName;
+                _excelHeader++;
+            }
+            // Остальная информация
+            for (int i = 0; i < DT.Rows.Count; i++)
+            {
+                _excelHeader = 'A';
+                int arrLength = DT.Rows[i].ItemArray.Length;
+                for (int j = 0; j < arrLength; j++)
+                {
+                    Sheet.Cells[i + 2, j] = DT.Rows[i].ItemArray[j];
+                }
+                _excelHeader++;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+
+    /// <summary>
+    /// Освобождение ресурсов, закрытие рабочей книги
+    /// </summary>
     public void Dispose()
     {
         try
         {
-            workbook.Close();
+            Workbook.Close();
+            app.Quit();
         }
         catch (Exception ex) { Console.WriteLine(ex.Message); }
     }
+
+    /// <summary>
+    /// Закрытие приложения (ручной запуск)
+    /// </summary>
+    protected internal void Quit() => app.Quit();
 }
 
--- CompareExel/Program.cs
+++ CompareExel/Program.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Diagnostics;
-using System.IO;
-using ExcelDataReader;
+using System.Text.RegularExpressions;
 
 namespace CompareExcel;
 
 class Program
 {
+    /// <summary>
+    /// Директория извлекаемых таблиц
+    /// </summary>
+    static public string directory = @"C:\Users\pvslavinsky\Desktop\ФКР\Результаты\Постановление о запрете на регистрационные действия в отношении транспортных средств";
+
     static void Main(string[] args)
     {
-        Stopwatch sw_total = new Stopwatch();
-        sw_total.Start(); // Таймер
+        Stopwatch timer = new();
+        timer.Start(); // Таймер
 
-        sw_total.Stop();
+        string[] filesDir = Directory.GetFiles(directory, "*.xlsx"); // Сканирует файлы формата Excel
+
+        using (ExcelUse ex = new())
+        {
+            DataTable dt = ex.ReadFile($@"{directory}\piev_65011250351440.xlsx");
+            ex.Convert(dt);
+            ex.SaveAs(@"C:\Users\pvslavinsky\Desktop\ФКР\Результаты\Full", GetFileName(directory));
+        }
+
+        timer.Stop();
+        Console.WriteLine($"Директория: {directory}\n Потрачено времени: {timer.ElapsedMilliseconds} ms.");
     }
 
     /// <summary>
@@ -22,7 +36,7 @@ class Program
     /// </summary>
     /// <param name="dirName">Путь к папке</param>
     /// <returns></returns>
-    private List<DataTable> ReadRange(string dirName)
+    static List<DataTable> ReadRange(string dirName)
     {
         string[] filesDir = Directory.GetFiles(dirName, "*.xlsx"); // Сканирует файлы формата Excel
 
@@ -30,14 +44,32 @@ class Program
         {
             try
             {
-                using (var excelApp = new ExcelUse())
-                {
-                    excelApp.Open(filePatch);
-                }
+                using ExcelUse excelApp = new();
+                excelApp.ReadFile(filePatch);
             }
             catch (Exception ex) { Console.WriteLine(ex.Message); }
         }
 
         return null;
+    }
+
+    /// <summary>
+    /// Создание имени файла
+    /// </summary>
+    /// <param name="DirFolder">Директория извлекаемых таблиц</param>
+    /// <returns></returns>
+    private static string GetFileName(string DirFolder) => GetFileName(DirFolder, DateTime.Now.Month, DateTime.Now.Year);
+
+    /// <summary>
+    /// Создание имени файла
+    /// </summary>
+    /// <param name="DirFolder">Директория извлекаемых таблиц</param>
+    /// <param name="mounth">Месяц</param>
+    /// <param name="year">Год</param>
+    /// <returns></returns>
+    private static string GetFileName(string DirFolder, int mounth, int year)
+    {
+        // Если путь с названием файла, то берется вторая подстрочка с конца (имя папки)
+        return $"{DirFolder.Split(@"\")[^(DirFolder.EndsWith(".xlsx") ? 2 : 1)]}_{mounth}_{year}";
     }
 }
